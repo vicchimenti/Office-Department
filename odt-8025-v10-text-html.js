8e0a1b92e151757b50cbf8e0c767d8378bcb4b50
Vic Chimenti <victor.chimenti@gmail.com>
2024-10-29 14:42:05 -0700

    fa-brands fa-square-threads

--- odt-8025-v10-text-html.js
+++ odt-8025-v10-text-html.js
@@ -340,11 +340,19 @@ try {
         '</a>' +
         '</li>' :
         '<span class="hidden instagramSocial"></span>';
+    let threadsString = (socialTrue && officeDict.threads.content) ?
+        '<li>' +
+        '<a href="' + officeDict.threads.content + '" title="Instagram" aria-label="Instagram opens in a new tab" target="_blank">' +
+        '<span class="show-for-sr">Instagram</span>' +
+        '<span class="fa-brands fa-square-threads" aria-hidden="true"></span>' +
+        '</a>' +
+        '</li>' :
+        '<span class="hidden instagramSocial"></span>';
 
         
 
 
-
+<i class="fa-brands fa-square-threads"></i>
 
 
 
