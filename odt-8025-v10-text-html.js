fb1f403a3f6521973b2e02c644e10f526a3349d5
Vic Chimenti <victor.chimenti@gmail.com>
2024-10-29 09:09:53 -0700

    facebook

--- odt-8025-v10-text-html.js
+++ odt-8025-v10-text-html.js
@@ -207,6 +207,7 @@ try {
         email: getContentValues('<t4 type="content" name="Email" output="normal" modifiers="striptags,htmlentities,encode_emails" />'),
         location: getContentValues('<t4 type="content" name="Location" output="normal" modifiers="striptags,htmlentities" />'),
         openingHours: getContentValues('<t4 type="content" name="Opening Hours" output="normal" modifiers="striptags,htmlentities" />'),
+        facebook: getContentValues('<t4 type="content" name="Facebook URL" output="normal" modifiers="striptags,htmlentities" />'),
 
 
 
