108f8ec62905a7cf1ca2725bc6287d09cfd00d5c
Vic Chimenti <victor.chimenti@gmail.com>
2024-10-30 10:35:13 -0700

    Office/Dept Image

--- odt-8025-v10-text-html.js
+++ odt-8025-v10-text-html.js
@@ -89,7 +89,7 @@ function mediaTag(mediaPath, mediaPathFull) {
 
     let imageW1 = ' 360w, ';
     let imageW2 = ' 728w';
-    let itemId = content.get('Media Library Image').getID();
+    let itemId = content.get('Office/Dept Image').getID();
     let mediaInfo = getMediaInfo(itemId);
     let media = readMedia(itemId);
     let info = new ImageInfo();
